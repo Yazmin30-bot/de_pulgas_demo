95a11b76711371a7d36edd3cdc64c8030aac95a7
Yazmin Tinoco <ytip30@gmail.com>
2021-08-18 19:39:42 -0500

    Service worked dont send

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
-});
+/* app.get("/serviceWorker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+}); */
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
